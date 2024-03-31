1cf116d8c4c100d6010d1cd805cd3bbcea48e3da
Pawel Zelmanski <pzelmanski1@gmail.com>
2024-03-31 13:21:00 +0200

    add mapping to close barbar with <space>q

--- lua/pawel/config/keymaps.lua
+++ lua/pawel/config/keymaps.lua
@@ -9,3 +9,4 @@ keymap.set("v", "<leader>y", "\"*y", {desc = "Copy selection to clipboard" })
 local opts = { noremap = true, silent = true }
 keymap.set("n", "<Tab>", "<Cmd>BufferNext<CR>", opts)
 keymap.set("n", "<S-Tab>",  "<Cmd>BufferPrevious<CR>", opts)
+keymap.set("n", "<leader>q", "<Cmd>BufferClose<CR>", opts)
